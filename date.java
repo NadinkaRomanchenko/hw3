1a30834d54e736c4421612a7a7c6a88f82c470b1
Nadin4ik <67558617+Nadin4ik@users.noreply.github.com>
2020-06-29 00:06:17 +0300

    Add files via upload

---
+++ date.java
@@ -0,0 +1,48 @@
+
+//Имеются три числа - день, месяц и год, вводимые пользователем с консоли.
+//        Вывести дату следующего дня в формате "День.Месяц.Год".
+//        Учесть переход на следующий месяц, а также следующий год.
+//         Форматирование строки "День.Месяц.Год" вынести в отдельную функцию.
+
+
+public class date {
+
+    public static void main(String[] args) {
+        int day = 33;
+        int month = 12;
+        int year = 2020;
+        System.out.println(day + "." + month + "." + year);
+        if (day < 28) {
+            day = day + 1;
+            System.out.println(day + "." + month + "." + year);
+        } else if (day == 28 && month == 2) {
+            day = 1;
+            month = 3;
+            System.out.println(day + "." + month + "." + year);
+        } else if (day == 28 && (month == 1 || (month >= 3 && month <= 12))) {
+            day = day + 1;
+            System.out.println(day + "." + month + "." + year);
+        } else if (day == 29 && (month != 2)) {
+            day = day + 1;
+            System.out.println(day + "." + month + "." + year);
+        } else if (day == 30 && (month == 4 || month == 6 || month == 9 || month == 11)) {
+            day = 1;
+            month = month + 1;
+            System.out.println(day + "." + month + "." + year);
+        } else if (day == 30 && (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12)) {
+            day = day + 1;
+            System.out.println(day + "." + month + "." + year);
+        } else if (day == 31 && (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10)) {
+            day = 1;
+            month = month + 1;
+            System.out.println(day + "." + month + "." + year);
+        } else if (day == 31 && month == 12) {
+            day = 1;
+            month = 1;
+            year = year + 1;
+            System.out.println(day + "." + month + "." + year);
+        } else {
+            System.out.println("дата введена некоректно");
+        }
+    }
+}
